91f6f3fc4eb5afefb4f93cae98cde7617c53208b
Matthew (Matt) Hegi <83738750+Matthegi@users.noreply.github.com>
2024-02-25 02:13:29 +0000

    added secure websockets

--- recommender/src/main/java/com/mm/v2/RequestReciever.java
+++ recommender/src/main/java/com/mm/v2/RequestReciever.java
@@ -1,22 +1,50 @@
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.KeyManagerFactory;
+import java.io.FileInputStream;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 
-public class RequestReciever extends WebSocketServer {
+public class RequestReceiver extends WebSocketServer {
 
-    public RequestReciever(InetSocketAddress address) {
+    public RequestReceiver(InetSocketAddress address) {
         super(address);
+        setUpSSL();
+    }
+
+    private void setUpSSL() {
+        try {
+            String STORETYPE = "JKS";
+            String KEYSTORE = "../../../../../keystore.jks"; // Path to your keystore file
+            String STOREPASSWORD = "b3LukeMattTom"; // Keystore password
+            String KEYPASSWORD = "b3LukeMattTom"; // Key password
+
+            KeyStore ks = KeyStore.getInstance(STORETYPE);
+            File kf = new File(KEYSTORE);
+            ks.load(new FileInputStream(kf), STOREPASSWORD.toCharArray());
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, KEYPASSWORD.toCharArray());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), null, null);
+
+            setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         System.out.println("New connection: " + conn.getRemoteSocketAddress());
-        conn.send("Welcome to the WebSocket server!"); // Send a welcome message
+        conn.send("Welcome to the WebSocket server!");
     }
 
-    @Override
+     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         System.out.println("Closed connection: " + conn.getRemoteSocketAddress());
     }
@@ -38,12 +66,11 @@ public class RequestReciever extends WebSocketServer {
     }
 
     public static void main(String[] args) {
-        String host = "0.0.0.0"; // Bind to all interfaces
-        int port = 8080; // Specify your desired port
+        String host = "0.0.0.0";
+        int port = 8080;
 
-        WebSocketServer server = new RequestReciever(new InetSocketAddress(host, port));
+        WebSocketServer server = new RequestReceiver(new InetSocketAddress(host, port));
         server.start();
         System.out.println("The WebSocket server started on port: " + server.getPort());
     }
-
 }
